9f8f98b78ca76408de71db89fc2408da292e6286
Sara Roa <sorjuelar@unbosque.edu.co>
2026-02-28 23:46:26 -0500

    Practica con el profesor en clase

---
+++ javascript/types.js
@@ -0,0 +1,11 @@
+const nombre = "Sara Valentina Orjuela Roa";
+const numero = 1025523067;
+const soyEstudiante = true;
+let sinDefinicion;
+const nula = null;
+
+console.log(nombre);
+console.log(numero);
+console.log(soyEstudiante);
+console.log(sinDefinicion);
+console.log(nula);
