24577c7a9b1784937e8a4f6263510df1be3388f6
DSI3-202601-SVOROA <sorjuelar@unbosque.edu.co>
2026-04-24 20:39:27 -0500

    Delete coffee directory

--- coffee/src/app/components/atoms/error-message/error-message.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-error-message',
-  imports: [],
-  templateUrl: './error-message.html',
-  styleUrl: './error-message.css',
-})
-export class ErrorMessage {}
--- coffee/src/app/components/molecules/login-header/login-header.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-login-header',
-  imports: [],
-  templateUrl: './login-header.html',
-  styleUrl: './login-header.css',
-})
-export class LoginHeader {}
